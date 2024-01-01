80a6d612c4eabb730a16383f839fa691f823a91b
wlfbbz <44901408+wlfbbz@users.noreply.github.com>
2024-01-01 14:17:55 +0000

    - hid form after submission - changed message width to 450px - added a loading state for button - changed mobile message width 340px, line height to 24px and font size 12px

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     var signupForm = document.getElementById('signupForm');
+    var submitButton = signupForm.querySelector('button');
     var messageDiv = document.getElementById('message');
 
     signupForm.addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent default form submission
+        submitButton.classList.add('loading');
+        submitButton.disabled = true; // Disable the button to prevent multiple submissions
+        submitButton.textContent = 'Submitting...'; // Change button text
 
         var formData = new FormData(signupForm);
         var request = new XMLHttpRequest();
         request.open('POST', signupForm.action, true);
 
         request.onload = function() {
+            submitButton.classList.remove('loading');
+            submitButton.disabled = false; // Re-enable the button
+
             if (request.status >= 200 && request.status < 400) {
                 var resp = JSON.parse(request.responseText);
                 if(resp.result === "success") {
-                    messageDiv.innerHTML = "Thank you for signing up! Happy New Year ✨";
+                    signupForm.style.display = 'none'; // Hide the form
+                    messageDiv.innerHTML = "Thank you for signing up! We will notify you know when it's out. Happy New Year ✨";
                     messageDiv.style.display = 'block'; // Show the message
-                    signupForm.reset(); // Reset form here
                 } else {
                     messageDiv.innerHTML = "Submission failed. Please try again.";
                     messageDiv.style.display = 'block';
@@ -24,9 +31,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 messageDiv.innerHTML = "Error submitting form. Please try again.";
                 messageDiv.style.display = 'block';
             }
+
+            submitButton.textContent = 'Sign up to be first to know'; // Reset button text
         };
 
         request.onerror = function() {
+            submitButton.classList.remove('loading');
+            submitButton.disabled = false; // Re-enable the button
+            submitButton.textContent = 'Sign up to be first to know'; // Reset button text
+
             messageDiv.innerHTML = "Error submitting form. Please check your connection and try again.";
             messageDiv.style.display = 'block';
         };
